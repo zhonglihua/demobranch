e9d1257baea51d0f3b3ea468d0d766f4db8db78f
senxu <sen.xu@dragonflow.com>
2012-04-25 10:01:29 +0800

    dd

---
+++ src/demo/Next.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Next {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is example");
+	}
+
+}
