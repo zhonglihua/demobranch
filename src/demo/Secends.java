1f5cbf69e73e0e422050147c85e27b1b0f31b326
senxu <sen.xu@dragonflow.com>
2012-04-25 10:10:49 +0800

    ddd

---
+++ src/demo/Secends.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Secends {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xdjhk");
+	}
+
+}
